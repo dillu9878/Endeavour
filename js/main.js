73e96cbc90ef19888c889274328d0347e947bc30
dillu9878 <dillu9878@gmail.com>
2019-08-20 10:56:34 +0530

    endeavour_website

--- js/main.js
+++ js/main.js
@@ -16,8 +16,67 @@ $(document).ready(function(){
         
         else{
             $(".nav").removeClass("sticky");
+        }  
+    });
+    
+    $('.bxslider').bxSlider({
+            mode: 'horizontal',
+            moveSlides: 1,
+            slideMargin: 40,
+            infiniteLoop: true,
+            minSlides: 1,
+            maxSlides: 1,
+            speed: 1200,
+            });
+    
+    if($(".swiper-container").hasClass("team-member-slider")){
+        var swiper = new Swiper('.swiper-container', {
+            sliderPerView:3,
+            allowTouchMove: true,
+            loop: true,
+            centeredSlides: true,
+            slideToclickedslide: true,
+            effect: "coverflow",
+            grabcursor: true,
+            autoplay: false,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            coverflow: {
+                rotate: 0,
+                stretch: 100,
+                depth: 200,
+                modifier: 1,
+                slideShadows: false
+            },
+            breakpoints:{
+                767: {
+                    slidesPerView: 1,
+                    centeredSlides:false,
+                    effect: "slide",    
+                }
+            }
+        });
+    }
+
+    
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
         }
     });
     
+    $("#news-slider").owlCarousel({
+        items: 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,2],
+        itemsMobile:[599,1],
+        pagination: false,
+        navigationText: false,
+        autoplay: true
+    });
     
 });
